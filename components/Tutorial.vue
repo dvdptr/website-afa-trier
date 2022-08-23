d26d806deb91cf6d7f5f4d8a6d01fa0ec5218a78
dvdptr <info@davidebner.de>
2022-08-23 10:34:02 +0200

    init

---
+++ components/Tutorial.vue
@@ -0,0 +1,12 @@
+<!-- Please remove this file from your project -->
+<template>
+   <h1 class="   text-xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
