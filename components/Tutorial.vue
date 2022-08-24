4b26d6a669f6ad44ce08b0c9d62e63a10075b3f1
dvdptr <info@davidebner.de>
2022-08-24 09:20:23 +0200

    add and run tailwindcss prettier plugin

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,12 +1,10 @@
 <!-- Please remove this file from your project -->
 <template>
-   <h1 class="   text-xl font-bold underline">
-    Hello world!
-  </h1>
+  <h1 class="text-xl font-bold underline">Hello world!</h1>
 </template>
 
 <script>
 export default {
-  name: 'NuxtTutorial'
-}
+  name: "NuxtTutorial",
+};
 </script>
